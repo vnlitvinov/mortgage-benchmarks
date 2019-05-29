ab8b9a236edd14cca9585b7b23281918bde768dd
Anatoly Myachev <anatoly.myachev@intel.com>
2019-05-29 12:37:42 +0300

    removed 'git clone ..' inside test_mortgage.sh

--- test_mortgage_gpu.sh
+++ test_mortgage_gpu.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-mkdir mortgage_gpu && cd mortgage_gpu
+mkdir test_mortgage_gpu && cd test_mortgage_gpu
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+rm Miniconda3-latest-Linux-x86_64.sh
 export PATH="$HOME/miniconda/bin:$PATH"
 
-conda create -n cudf_dev  -c nvidia -c rapidsai -c numba -c conda-forge -c defaults cudf=0.7 python=3.6 cudatoolkit=9.2 -y
+conda create -n cudf_dev  -c nvidia -c rapidsai -c numba -c conda-forge -c defaults cudf=0.7 python=3.6 cudatoolkit=9.2 xgboost -y
 conda init bash
-source ~/.bashrc
 conda activate cudf_dev
-pip install xgboost
 
 mkdir dataset && cd dataset
 wget http://rapidsai-data.s3-website.us-east-2.amazonaws.com/notebook-mortgage-data/mortgage_2000-2001.tgz
 tar -xzvf mortgage_2000-2001.tgz
-cd ..
+cd ../..
 
-git clone https://github.com/vnlitvin/mortgage-benchmarks.git
-cd mortgage-benchmarks
-git checkout mortgage_gpu
-
-time python Mortgage_GPU.py ../dataset/ 1
+# now in mortgage-benchmarks folder
+time python Mortgage_GPU.py  test_mortgage_gpu/dataset/ 1
 
